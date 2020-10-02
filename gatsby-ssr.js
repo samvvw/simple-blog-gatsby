be1b8c6635d2f02073e78bfe3cec6111bf986277
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-10-01 17:37:08 -0700

    Global styles

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,10 +5,26 @@
  */
 
 // You can delete this file if you're not using it
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
 import React from "react"
 
+const GlobalSyles = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body, html{
+    font-family: ${props => props.theme.fonts.main};
+    height: 100%;
+    background-color: ${props => props.theme.colors.light1};
+  }
+`
+
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={Theme}>
+    <GlobalSyles />
+    {element}
+  </ThemeProvider>
 )
